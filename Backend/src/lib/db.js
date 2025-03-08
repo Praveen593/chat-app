aa911562fa0fc4e25b9254f02b20ec31da958c34
Praveen593 <007praveen2003@gmail.com>
2025-03-08 22:48:52 +0530

    try to connect db

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+import {mongoose} from "mongoose";
 
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGOOSE_URI);
-        console.log(`MongoDB connected successfully :  ${conn.connection.host}`);
+        console.log(`MongoDB connected successfully :${conn.connection.host}`);
 
     } catch (error)
     {
